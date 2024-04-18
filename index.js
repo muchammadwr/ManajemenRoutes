09de301153fa556538b7ead5f3fa39157d13f6c1
muchammadwr <muchammadwr@gmail.com>
2024-04-18 10:44:09 +0700

    Session

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 // define port
 const port = 3000;
+const secret = "secret-key";
 
 // define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("secret-key"));
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 // define routes
 app.get("/signingcookie", (req, res) => {
@@ -19,6 +29,26 @@ app.get("/signingcookie", (req, res) => {
 app.get("/verifycookie", (req, res) => {
   const cookie = req.signedCookies;
   res.send(cookie);
+});
+
+app.get("/count", (req, res) => {
+  if (req.session.count) {
+    req.session.count++;
+  } else {
+    req.session.count = 1;
+  }
+
+  res.send(`count: ${req.session.count}`);
+});
+
+app.get("/register", (req, res) => {
+  const { username = "Anonim" } = req.query;
+  req.session.username = username;
+  res.redirect("/dashboard");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send(`Welcome back ${req.session.username}`);
 });
 
 app.use("/admin", require("./routes/admin"));
